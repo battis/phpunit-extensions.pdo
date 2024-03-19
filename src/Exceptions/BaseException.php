2de882b4b81296c068d900f6fc5b675728e2834f
battis <seth@battis.net>
2024-03-19 17:29:01 +0000

    icon

---
+++ src/Exceptions/BaseException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Battis\PHPUnit\PDO\Exceptions;
+
+/**
+ * @package battis\phpunit-pdo
+ */
+class BaseException extends FixtureException
+{
+}
---
+++ src/Exceptions/SchemaException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Battis\PHPUnit\PDO\Exceptions;
+
+/**
+ * @package battis\phpunit-pdo
+ */
+class SchemaException extends BaseException
+{
+}
